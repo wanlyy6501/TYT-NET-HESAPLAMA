bce11d63c6b80a25eb67f33a9019aef4ad55611d
wanlyy6501 <129601948+wanlyy6501@users.noreply.github.com>
2023-04-01 16:48:32 +0300

    TYT NET HESAPLAMA
    NET HESAPLAMA

---
+++ Form1.Designer.cs
@@ -0,0 +1,331 @@
+﻿namespace Tyt_Ayt_Net_Hesaplama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblTN = new System.Windows.Forms.Label();
+            this.txtTD = new System.Windows.Forms.TextBox();
+            this.txtTY = new System.Windows.Forms.TextBox();
+            this.txtTN = new System.Windows.Forms.TextBox();
+            this.txtSN = new System.Windows.Forms.TextBox();
+            this.txtSD = new System.Windows.Forms.TextBox();
+            this.txtSY = new System.Windows.Forms.TextBox();
+            this.txtMD = new System.Windows.Forms.TextBox();
+            this.txtMY = new System.Windows.Forms.TextBox();
+            this.txtMN = new System.Windows.Forms.TextBox();
+            this.txtFD = new System.Windows.Forms.TextBox();
+            this.txtFY = new System.Windows.Forms.TextBox();
+            this.txtFN = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(33, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Doğru";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(33, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Yanlış";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(54, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Net";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(106, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Türkçe (40)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(239, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 20);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Sosyal (20)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(355, 15);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(129, 20);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Matematik (40)";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(502, 15);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(77, 20);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Fen (20)";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(584, 185);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 43);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Puan Hesapla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(312, 192);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(136, 24);
+            this.label8.TabIndex = 20;
+            this.label8.Text = " Toplam Net :";
+            // 
+            // lblTN
+            // 
+            this.lblTN.AutoSize = true;
+            this.lblTN.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTN.Location = new System.Drawing.Point(375, 171);
+            this.lblTN.Name = "lblTN";
+            this.lblTN.Size = new System.Drawing.Size(0, 24);
+            this.lblTN.TabIndex = 21;
+            // 
+            // txtTD
+            // 
+            this.txtTD.Location = new System.Drawing.Point(120, 45);
+            this.txtTD.Multiline = true;
+            this.txtTD.Name = "txtTD";
+            this.txtTD.Size = new System.Drawing.Size(53, 31);
+            this.txtTD.TabIndex = 23;
+            this.txtTD.Text = "0";
+            this.txtTD.TextChanged += new System.EventHandler(this.txtTD_TextChanged);
+            // 
+            // txtTY
+            // 
+            this.txtTY.Location = new System.Drawing.Point(120, 89);
+            this.txtTY.Multiline = true;
+            this.txtTY.Name = "txtTY";
+            this.txtTY.Size = new System.Drawing.Size(53, 31);
+            this.txtTY.TabIndex = 25;
+            this.txtTY.Text = "0";
+            this.txtTY.TextChanged += new System.EventHandler(this.txtTY_TextChanged);
+            // 
+            // txtTN
+            // 
+            this.txtTN.Location = new System.Drawing.Point(120, 136);
+            this.txtTN.Multiline = true;
+            this.txtTN.Name = "txtTN";
+            this.txtTN.Size = new System.Drawing.Size(53, 31);
+            this.txtTN.TabIndex = 26;
+            this.txtTN.Text = "0";
+            // 
+            // txtSN
+            // 
+            this.txtSN.Location = new System.Drawing.Point(252, 136);
+            this.txtSN.Multiline = true;
+            this.txtSN.Name = "txtSN";
+            this.txtSN.Size = new System.Drawing.Size(53, 31);
+            this.txtSN.TabIndex = 27;
+            this.txtSN.Text = "0";
+            // 
+            // txtSD
+            // 
+            this.txtSD.Location = new System.Drawing.Point(252, 45);
+            this.txtSD.Multiline = true;
+            this.txtSD.Name = "txtSD";
+            this.txtSD.Size = new System.Drawing.Size(53, 31);
+            this.txtSD.TabIndex = 28;
+            this.txtSD.Text = "0";
+            this.txtSD.TextChanged += new System.EventHandler(this.txtSD_TextChanged);
+            // 
+            // txtSY
+            // 
+            this.txtSY.Location = new System.Drawing.Point(252, 89);
+            this.txtSY.Multiline = true;
+            this.txtSY.Name = "txtSY";
+            this.txtSY.Size = new System.Drawing.Size(53, 31);
+            this.txtSY.TabIndex = 29;
+            this.txtSY.Text = "0";
+            this.txtSY.TextChanged += new System.EventHandler(this.txtSY_TextChanged);
+            // 
+            // txtMD
+            // 
+            this.txtMD.Location = new System.Drawing.Point(369, 45);
+            this.txtMD.Multiline = true;
+            this.txtMD.Name = "txtMD";
+            this.txtMD.Size = new System.Drawing.Size(53, 31);
+            this.txtMD.TabIndex = 30;
+            this.txtMD.Text = "0";
+            this.txtMD.TextChanged += new System.EventHandler(this.txtMD_TextChanged);
+            // 
+            // txtMY
+            // 
+            this.txtMY.Location = new System.Drawing.Point(369, 89);
+            this.txtMY.Multiline = true;
+            this.txtMY.Name = "txtMY";
+            this.txtMY.Size = new System.Drawing.Size(53, 31);
+            this.txtMY.TabIndex = 31;
+            this.txtMY.Text = "0";
+            this.txtMY.TextChanged += new System.EventHandler(this.txtMY_TextChanged);
+            // 
+            // txtMN
+            // 
+            this.txtMN.Location = new System.Drawing.Point(369, 136);
+            this.txtMN.Multiline = true;
+            this.txtMN.Name = "txtMN";
+            this.txtMN.Size = new System.Drawing.Size(53, 31);
+            this.txtMN.TabIndex = 32;
+            this.txtMN.Text = "0";
+            // 
+            // txtFD
+            // 
+            this.txtFD.Location = new System.Drawing.Point(506, 45);
+            this.txtFD.Multiline = true;
+            this.txtFD.Name = "txtFD";
+            this.txtFD.Size = new System.Drawing.Size(53, 31);
+            this.txtFD.TabIndex = 33;
+            this.txtFD.Text = "0";
+            this.txtFD.TextChanged += new System.EventHandler(this.txtFD_TextChanged);
+            // 
+            // txtFY
+            // 
+            this.txtFY.Location = new System.Drawing.Point(506, 89);
+            this.txtFY.Multiline = true;
+            this.txtFY.Name = "txtFY";
+            this.txtFY.Size = new System.Drawing.Size(53, 31);
+            this.txtFY.TabIndex = 34;
+            this.txtFY.Text = "0";
+            this.txtFY.TextChanged += new System.EventHandler(this.txtFY_TextChanged);
+            // 
+            // txtFN
+            // 
+            this.txtFN.Location = new System.Drawing.Point(506, 136);
+            this.txtFN.Multiline = true;
+            this.txtFN.Name = "txtFN";
+            this.txtFN.Size = new System.Drawing.Size(53, 31);
+            this.txtFN.TabIndex = 35;
+            this.txtFN.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(707, 283);
+            this.Controls.Add(this.txtFN);
+            this.Controls.Add(this.txtFY);
+            this.Controls.Add(this.txtFD);
+            this.Controls.Add(this.txtMN);
+            this.Controls.Add(this.txtMY);
+            this.Controls.Add(this.txtMD);
+            this.Controls.Add(this.txtSY);
+            this.Controls.Add(this.txtSD);
+            this.Controls.Add(this.txtSN);
+            this.Controls.Add(this.txtTN);
+            this.Controls.Add(this.txtTY);
+            this.Controls.Add(this.txtTD);
+            this.Controls.Add(this.lblTN);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblTN;
+        private System.Windows.Forms.TextBox txtTD;
+        private System.Windows.Forms.TextBox txtTY;
+        private System.Windows.Forms.TextBox txtTN;
+        private System.Windows.Forms.TextBox txtSN;
+        private System.Windows.Forms.TextBox txtSD;
+        private System.Windows.Forms.TextBox txtSY;
+        private System.Windows.Forms.TextBox txtMD;
+        private System.Windows.Forms.TextBox txtMY;
+        private System.Windows.Forms.TextBox txtMN;
+        private System.Windows.Forms.TextBox txtFD;
+        private System.Windows.Forms.TextBox txtFY;
+        private System.Windows.Forms.TextBox txtFN;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tyt_Ayt_Net_Hesaplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double TD, TY, TN;
+            double SD, SY, SN;
+            double MD, MY, MN;
+            double FD, FY, FN;
+            double toplamnet = 0;
+
+
+            try
+            {
+                //Turkisch
+
+                TD = double.Parse(txtTD.Text);
+                TY = double.Parse(txtTY.Text);
+                TN = TD - (TY / 4);
+                double türkçetoplam = (TD + TY);
+
+                //Social
+
+                SD = double.Parse(txtSD.Text);
+                SY = double.Parse(txtTY.Text);
+                SN = SD - (SY / 4);
+                double sosyaltoplam = (SD + SY);
+
+                //Math
+
+                MD = double.Parse(txtMD.Text);
+                MY = double.Parse(txtMY.Text);
+                MN= MD - (MY / 4);
+                double mattoplam = (MD + MY);
+
+                // Science
+
+                FD = double.Parse(txtFD.Text);
+                FY = double.Parse(txtFY.Text);
+                FN = FD - (FY / 4);
+                double fentoplam = (FD + FY);
+
+
+                toplamnet = TN + SN + MN + FN;
+
+                txtTN.Text = TN.ToString();
+                txtSN.Text = SN.ToString();
+                txtMN.Text = MN.ToString();
+                txtFN.Text = FN.ToString();
+
+
+                if (türkçetoplam > 40)
+                {
+                    txtTD.Text = "0";
+                    txtTY.Text = "0";
+                    TN = 0;
+                    MessageBox.Show("Lütfen Verilerinizi Doğru Giriniz");
+                }
+
+
+                if (sosyaltoplam > 20)
+                {
+                    txtSD.Text = "0";
+                    txtSY.Text = "0";
+                    SN = 0;
+                    MessageBox.Show("Lütfen Verilerinizi Doğru Giriniz");
+                }
+
+                if (mattoplam > 40)
+                {
+                    txtMD.Text = "0";
+                    txtMY.Text = "0";
+                    MN = 0;
+                    MessageBox.Show("Lütfen Verilerinizi Doğru Giriniz");
+                }
+
+
+                if (fentoplam > 20)
+                {
+                    txtFD.Text = "0";
+                    txtFY.Text = "0";
+                    FN = 0;
+                    MessageBox.Show("Lütfen Verilerinizi Doğru Giriniz");
+                }
+
+                lblTN.Text = (TN + SN + MN + FN).ToString();
+
+            }
+            catch (Exception)
+            {
+             
+            }
+           
+        }
+
+        private void txtTD_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                double uzunluk1 = double.Parse(txtTD.Text);
+                if (uzunluk1>40)
+                {
+                    MessageBox.Show("Doğru Değer Giriniz");
+                    txtTD.Clear();
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void txtTY_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                double uzunluk2 = double.Parse(txtTY.Text);
+                if (uzunluk2 > 40)
+                {
+                    MessageBox.Show("Doğru Değer Giriniz");
+                    txtTY.Clear();
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void txtSD_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                double uzunluk3 = double.Parse(txtSD.Text);
+                if (uzunluk3 > 20)
+                {
+                    MessageBox.Show("Doğru Değer Giriniz");
+                    txtSD.Clear();
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void txtSY_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                double uzunluk4 = double.Parse(txtSY.Text);
+                if (uzunluk4 > 20)
+                {
+                    MessageBox.Show("Doğru Değer Giriniz");
+                    txtSY.Clear();
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void txtMD_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                double uzunluk5 = double.Parse(txtMD.Text);
+                if (uzunluk5 > 40)
+                {
+                    MessageBox.Show("Doğru Değer Giriniz");
+                    txtMD.Clear();
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void txtMY_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                double uzunluk6 = double.Parse(txtMY.Text);
+                if (uzunluk6 > 40)
+                {
+                    MessageBox.Show("Doğru Değer Giriniz");
+                    txtMY.Clear();
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void txtFD_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                double uzunluk7 = double.Parse(txtFD.Text);
+                if (uzunluk7 > 20)
+                {
+                    MessageBox.Show("Doğru Değer Giriniz");
+                    txtFD.Clear();
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void txtFY_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                double uzunluk8 = double.Parse(txtFY.Text);
+                if (uzunluk8 > 20)
+                {
+                    MessageBox.Show("Doğru Değer Giriniz");
+                    txtFY.Clear();
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
